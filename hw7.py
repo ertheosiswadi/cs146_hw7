4a354eb66751d7d4a44f3615a84f571066f48335
Ertheo Siswadi <esiswadi@g.ucla.edu>
2019-05-25 02:13:23 -0700

    almost finished

--- hw7.py
+++ hw7.py
@@ -3,6 +3,9 @@ import sys
 from PIL import Image
 from numpy import array
 from numpy.linalg import norm
+from math import pow
+
+k = 16
 
 bear = Image.open('pic.jpg')
 pix_bear = bear.load()
@@ -10,43 +13,41 @@ print(bear.size)
 print(pix_bear[0,0])
 
 #find the 4 means
-m = [array([147, 200, 250]), array([0, 0, 0]), array([137, 141,  57]), array([ 31,  70, 125])]
+m = [array([147, 200, 250])] + [array([0,0,0]) for i in range(k-1)]
+# m = [array([147, 200, 250]), array([0, 0, 0]), array([137, 141,  57]), array([ 31,  70, 125])]
 
-# #get distance of closest mu
-# #param: 
-# #   rgb - the norm we want to compare against
-# #   n - the first mus we want to consider
-# def get_closest(rgb, n):
-#     global m
-#     #initialize to_return
-#     to_return = norm(rgb-m[0])
-#     for i in range(n):
-#         if norm(rgb-m[i]) < to_return:
-#             to_return = norm(rgb-m[i])
-#     return to_return
+#get distance of closest mu
+#param: 
+#   rgb - the norm we want to compare against
+#   n - the first mus we want to consider
+def get_closest(rgb, n):
+    global m
+    #initialize to_return
+    to_return = norm(rgb-m[0])
+    for i in range(n):
+        if norm(rgb-m[i]) < to_return:
+            to_return = norm(rgb-m[i])
+    return to_return
 
-# #given an m with an initialized m[0]
-# for n in range(1,4):
-#     max_dist = -1
-#     for i in range(bear.size[0]):
-#         for j in range(bear.size[1]):
-#             #find the closest mu we already have to the point i, j
-#             #if the closest mu to the point distance is bigger than the current mu
-#                 #set it to the current mu
-#             closest = get_closest(array(pix_bear[i,j]),n)
-#             if closest > max_dist:
-#                 # print('closest:{}, max_dist:{}'.format(closest, max_dist))
-#                 max_dist = closest
-#                 m[n] = array(pix_bear[i,j])
-#                 # print('m1-{}, i:{}, j:{}'.format(m[n],i,j))
-# print(m)
+#given an m with an initialized m[0]
+for n in range(1,k):
+    max_dist = -1
+    for i in range(bear.size[0]):
+        for j in range(bear.size[1]):
+            #find the closest mu we already have to the point i, j
+            #if the closest mu to the point distance is bigger than the current mu
+                #set it to the current mu
+            closest = get_closest(array(pix_bear[i,j]),n)
+            if closest > max_dist:
+                # print('closest:{}, max_dist:{}'.format(closest, max_dist))
+                max_dist = closest
+                # print('lenm:{} n:{}'.format(len(m),n))
+                m[n] = array(pix_bear[i,j])
+                # print('m1-{}, i:{}, j:{}'.format(m[n],i,j))
+print('Initial mus: {}'.format(m))
 
 #list of set
-m_set = [[[-1,-1]],
-        [[-1,-1]],
-        [[-1,-1]],
-        [[-1,-1]]]
-
+m_set = [[[-1,-1]] for i in range(k)]
 #return an index to m
 def get_closest_m(rgb):
     global m
@@ -61,7 +62,10 @@ def get_closest_m(rgb):
 #compare each pixel with all the mus, see which yields the smallest distance
 #   then add the pixel's coordinates to the m_x_set
 
+J = []
+
 for z in range(10):
+    m_set = [[[-1,-1]] for i in range(k)]
     for i in range(bear.size[0]):
         for j in range(bear.size[1]):
             index = get_closest_m(array(pix_bear[i,j]))
@@ -82,3 +86,21 @@ for z in range(10):
         m[i] = vec_sum / len(m_set[i])
 
     print(m)
+
+    #each iteration calculate the Objective function
+    #   this is simply the some of all the norms 
+    
+    j = 0
+    for i in range(len(m_set)):
+        for e in m_set[i]:
+            j += pow(norm(pix_bear[e[0],e[1]]-m[i]),2)
+    J.append(int(round(j)))
+
+print('Objective Function List: {}'.format(J))
+
+#Perform the compression
+for i in range(len(m_set)):
+    for e in m_set[i]:
+        pix_bear[e[0],e[1]] = tuple([int(j) for j in m[i]])
+
+bear.save('./new_bear_k{}.jpg'.format(k))
